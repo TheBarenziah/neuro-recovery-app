5a948f53d73a061b1c7533b1efb9e9b58c65e7e8
Joseph Killen <jkillen101@hotmail.com>
2024-05-03 17:13:37 +0100

    email and password validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -14,6 +14,7 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
 import { MatButton } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { DatabaseService } from '../database.service';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -34,27 +35,54 @@ export class RegisterComponent {
     username: ['', Validators.required],
     email: this.fb.control<string>('', [Validators.required, Validators.email]),
     password: ['', Validators.required],
-  });
+    confirmPassword: ['', Validators.required]
+  }, { validators: RegisterComponent.passwordMatchValidator });
 
   errorMessage: string | null = null;
 
+  static passwordMatchValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+    const formGroup = control as FormGroup;
+    const password = formGroup.get('password')?.value;
+    const confirmPassword = formGroup.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { notMatched: true };
+  };
+  
+
+  // onSubmit(): void {
+  //   const { email, username, password } = this.form.getRawValue();
+  //   this.authService.register(email, username, password).subscribe({
+  //     next: (user) => {
+  //       // Extract relevant data to be saved in the database
+  //       const userData = { email, username };
+  //       this.dbService.set(`users/${user.uid}`, userData)
+  //         .then(() => {
+  //           this.router.navigateByUrl('/home');
+  //         })
+  //         .catch(error => console.error('Error saving user details:', error));
+  //     },
+  //     error: (err) => {
+  //       this.errorMessage = err.code;
+  //     }  
+  //   });
+  // }
+
   onSubmit(): void {
-    const { email, username, password } = this.form.getRawValue();
-    this.authService.register(email, username, password).subscribe({
-      next: (user) => {
-        // Extract relevant data to be saved in the database
-        const userData = { email, username };
-        this.dbService.set(`users/${user.uid}`, userData)
-          .then(() => {
-            this.router.navigateByUrl('/home');
-          })
-          .catch(error => console.error('Error saving user details:', error));
-      },
-      error: (err) => {
-        this.errorMessage = err.code;
-      }  
-    });
+    if (this.form.valid) {
+      const { email, username, password } = this.form.getRawValue();
+      this.authService.register(email, username, password).subscribe({
+        next: (user) => {
+          this.dbService.set(`users/${user.uid}`, { email, username })
+            .then(() => this.router.navigateByUrl('/home'))
+            .catch(error => console.error('Error saving user details:', error));
+        },
+        error: (err) => this.errorMessage = err.code
+      });
+    } else {
+      // Handle the case when form is not valid
+      console.error('Form is not valid');
+    }
   }
+  
 
   hide = true;
 
